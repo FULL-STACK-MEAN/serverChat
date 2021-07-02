3cde77ff364b3e4dd11bac0829d5a7abc5843489
getafemean <getafemean@gmail.com>
2021-07-02 20:12:54 +0200

    bidireccional

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 io.on("connection", socket => {
-   console.log(socket.id);
+    console.log(socket.id);
+
+    socket.on('messageChat', data => {
+        console.log(JSON.parse(data));
+        io.emit('messageChat', JSON.stringify({text: 'Hola Mundo desde el Servidor!'}))
+    })
 
     socket.on("disconnect", () => {
         console.log('Se ha desconectado ' + socket.id);
