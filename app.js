18428acf8e94e02235445ec44bd96dd529bdf847
getafemean <getafemean@gmail.com>
2021-07-02 18:52:41 +0200

    connections

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const app = express();
 
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
+io.on("connection", socket => {
+   console.log(socket.id);
+
+    socket.on("disconnect", () => {
+        console.log('Se ha desconectado ' + socket.id);
+    })
+
+})
 
 
 
 
 const port = 3000;
-app.listen(port, () => {
+http.listen(port, () => {
     console.log('Servidor escuchando en http://localhost:' + port);
 })
