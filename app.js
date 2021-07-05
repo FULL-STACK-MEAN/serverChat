711d59cccf5c6a9acbd0ce2e103025dc53c6b493
getafemean <getafemean@gmail.com>
2021-07-05 17:11:49 +0200

    end

--- app.js
+++ app.js
@@ -7,17 +7,45 @@ app.use(cors());
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+let users = [];
+
 io.on("connection", socket => {
-    console.log(socket.id);
+    
+    socket.on('start', data => {
+        let user = {};
+        user.id = socket.id;
+        user.name = (JSON.parse(data)).name;
+        users.push(user);
+        let responseData = {
+            label: 'start',
+            users
+        }
+        io.emit('start', JSON.stringify(responseData));
+    })
 
     socket.on('messageChat', data => {
         const messageChat = JSON.parse(data);
-        //....
-        io.emit('messageChat', JSON.stringify(messageChat))
+        let responseData = {
+            label: 'messageChat',
+            message: data
+        }
+        io.emit('messageChat', JSON.stringify(responseData))
     })
 
     socket.on("disconnect", () => {
-        console.log('Se ha desconectado ' + socket.id);
+        let userName;
+        users.forEach((elem, i) => {
+            if(elem.id === socket.id) {
+                userName = elem.name;
+                users.splice(i, 1);
+            }
+        })
+        let responseData = {
+            label: 'end',
+            users,
+            userName
+        }
+        io.emit('end', JSON.stringify(responseData))
     })
 
 })
